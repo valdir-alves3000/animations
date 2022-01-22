0ff74e7eab87a3955e74477990c7f9bd75e4a733
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2022-01-22 15:22:24 -0300

    Create Calculator

--- christmas/js/script.js
+++ christmas/js/script.js
@@ -7,10 +7,14 @@ const random = (min, max) => {
 for (let i = 1; i < 100; i++) {
   const star = document.createElement("div");
   const snow = document.createElement("div");
+  const size = random(10, 30);
 
   star.classList.add("small-star");
   star.style.setProperty("--left", random(-50, 150) + "%");
   star.style.animationDelay = 0.2 * i + "s";
+  star.style.animationDuration = random(1, 3) + "s";
+  star.style.width = size + "px";
+  star.style.height = size + "px";
 
   snow.classList.add("snow-fall");
   snow.style.setProperty("--left", random(0, 90) + "%");
